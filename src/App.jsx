ad3987b55fd34327a764b6cf1f8a87c9a0690b0b
Yehor Vahin <davyjohns322@gmail.com>
2025-04-30 18:53:23 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,27 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
+const App = () => {
   const [clicks, setClicks] = useState(() => {
-    const savedClicks = window.localStorage.getItem("saved-clicks")
-
+    const savedClicks = window.localStorage.getItem("saved-clicks");
     if (savedClicks !== null) {
-      return savedClicks;
+      return Number(savedClicks);
     }
-    return 0
-  })
+    return 0;
+  });
 
   useEffect(() => {
-    window.localStorage.setItem("saved clicks", clicks);
+    window.localStorage.setItem("saved-clicks", clicks);
   }, [clicks]);
 
   return (
-    <>
-      <div className="card">
+      <div>
         <button onClick={() => setClicks(clicks + 1)}>
           You clicked {clicks} times
         </button>
         <button onClick={() => setClicks(0)}>Reset</button>
       </div>
-      
-    </>
-  )
-}
+  );
+};  
 
 export default App
